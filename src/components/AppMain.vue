eea9c4bef531dfdcfb93e22deea7fcc9c6deb101
Polvere243 <danielercoli@gmail.com>
2024-02-03 22:05:27 +0100

    started adding style to main and imported it to App.vue

---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container relative">
+        <button class="series">CURRENT SERIES</button>
+        <div class="row">
+            <div class="card">
+                <figure></figure>
+                <figcaption></figcaption>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+/* contenitore */
+.container.relative {
+    position: relative;
+}
+</style>
