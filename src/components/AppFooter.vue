e3c436b1dbfb736be239dc9d01a38c540c022ff1
Polvere243 <danielercoli@gmail.com>
2024-02-04 18:33:59 +0100

    created blue bar

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,27 +11,32 @@ export default {
                 <li>
                     <a href="#">
                         <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-digital-comics.png" alt="kindle">
+                        <h4>DIGITAL COMICS</h4>
                     </a>
                 </li>
                 <li>
                     <a href="#">
                         <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-merchandise.png" alt="maglietta">
+                        <h4>DC MERCHANDISE</h4>
                     </a>
                 </li>
                 <li>
                     <a href="#">
                         <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-shop-locator.png" alt="tessere">
+                        <h4>SUBSCRIPTION</h4>
                     </a>
                 </li>
                 <li>
                     <a href="#">
                         <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-subscriptions.png"
                             alt="icona della posizione">
+                        <h4>COMIC SHOP LOCATOR</h4>
                     </a>
                 </li>
                 <li>
                     <a href="#">
                         <img class="visa" src="../assets/assets-vue-dc-comics-1/img/buy-dc-power-visa.svg" alt="carta visa">
+                        <h4>DC POWER VISA</h4>
                     </a>
                 </li>
             </ul>
@@ -125,8 +130,10 @@ export default {
 <style scoped>
 /* link alle sezioni del negozio */
 #shop-sections {
-    height: 200px;
+    height: 100px;
     background-color: dodgerblue;
+
+
 }
 
 .links-pic {
@@ -135,9 +142,24 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    height: 100%;
+    padding: 0 20px;
 }
 
-.visa {}
+.links-pic img {
+    max-width: 100%;
+    height: 35px;
+}
+
+.links-pic li a {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    text-decoration: none;
+    color: white;
+    font-size: 14px;
+}
+
 
 /* sfondo della sezione */
 
